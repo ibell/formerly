067525129155355cbf840afcd3bee0e28117a420
Ian Bell <ian.h.bell@gmail.com>
2019-03-02 13:57:09 -0700

    Package with WebUI; works but that package not very maintained

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # formerly
 An experiment with minimal services with a UI
+
+```
+pip install git+git://github.com/widdershin/flask-desktop.git
+python calculator.py
+```
--- calculator.py
+++ calculator.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
+from webui import WebUI # Add WebUI to your imports
 app = Flask(__name__)
+ui = WebUI(app, debug=True) # Create a WebUI instance
 
 @app.route('/')
 def frontend():
@@ -11,4 +13,5 @@ def calculate():
     return jsonify({'COP': float(values['Te']) + float(values['Tc'])})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    ui.run()
+    # app.run(host='0.0.0.0', debug=True)
