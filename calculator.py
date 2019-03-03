50ba4d6a0203b917c0a63ff5ad54e0be81c412ae
Ian Bell <ian.h.bell@gmail.com>
2019-03-03 12:56:14 -0700

    Saturation tables

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, current_app, url_for, render_template
 app = Flask(__name__)
+import CoolProp.CoolProp as CP
 
 @app.route('/')
 def frontend():
@@ -9,6 +10,35 @@ def frontend():
 def calculate():
     values = request.get_json()
     return jsonify({'COP': float(values['Te']) + float(values['Tc'])})
+
+@app.route('/sat_table', methods=['POST'])
+def sat_table():
+    values = request.get_json()
+    
+    Tvec = values['Tvec']
+    Q = values['Q']
+
+    # Setup
+    fluid = values['fluid']
+    backend = values.get('backend', 'HEOS')
+    AS = CP.AbstractState(backend, fluid)
+
+    # Map output string keys to enumerated keys 
+    output_keys = values['output_keys']
+    ikeys = {skey: CP.get_parameter_index(skey) for skey in output_keys}
+
+    # Calculate the outputs
+    outputs = {}
+    for T in Tvec:
+        AS.update(CP.QT_INPUTS, 0, T)
+        for output_key in output_keys:
+            val = AS.keyed_output(ikeys[output_key])
+            if output_key in outputs:
+                outputs[output_key].append(val)
+            else:
+                outputs[output_key] = [val]
+
+    return jsonify(outputs)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- test_calculate.py
+++ test_calculate.py
@@ -2,3 +2,7 @@ import requests
 res = requests.post('http://localhost:5000/calculate', json={"Te":"42.314", "Tc": "-1000"})
 if res.ok:
     print(res.json())
+
+res = requests.post('http://localhost:5000/sat_table', json={"Tvec":[300,400,500], "fluid":"Water", 'Q':0, 'output_keys':["Dmass","Dmolar"]})
+if res.ok:
+    print(res.json())
