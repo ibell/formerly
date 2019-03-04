06f5575e4c8140d1a263d8e8b3ac859d84c37048
Ian Bell <ian.h.bell@gmail.com>
2019-03-03 17:49:16 -0700

    Fix JWT calls

--- test_calculate_JWT.py
+++ test_calculate_JWT.py
@@ -1,12 +1,15 @@
 import requests
 
 with requests.Session() as s:
-    res = s.post('http://localhost:5000/login', json={"passkey":"trustme"})
+    res = s.post('http://localhost:23948/login', json={"passkey":"trustme"})
     if res.ok:
         token = res.json()['access_token']
+    else:
+        print(res.text())
+        quit()
     s.headers.update({'Authorization': 'Bearer %s' % token})
 
-    res = s.get('http://localhost:5000/calculate', json={'Te':300, 'Tc': '400a'})
+    res = s.get('http://localhost:23948/calculate', json={'Te':300, 'Tc': '400'})
     if res.ok:
         print(res.json())
     else:
