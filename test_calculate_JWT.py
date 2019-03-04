f9ca7f189911f39f8aae6e41696cc20f7e2ac3dd
Ian Bell <ian.h.bell@gmail.com>
2019-03-03 17:38:33 -0700

    Use a session

--- test_calculate_JWT.py
+++ test_calculate_JWT.py
@@ -1,14 +1,13 @@
 import requests
-res = requests.post('http://localhost:5000/login', json={"passkey":"trustme"})
-if res.ok:
-    token = res.json()['access_token']
-else:
-    print(res.text)
-    quit()
 
-res = requests.get('http://localhost:5000/calculate', json={'Te':300, 'Tc': 400}, headers={'Authorization': 'Bearer %s' % token})
-if res.ok:
-    print(res)
-    print(res.json())
-else:
-    print(res.text)
+with requests.Session() as s:
+    res = s.post('http://localhost:5000/login', json={"passkey":"trustme"})
+    if res.ok:
+        token = res.json()['access_token']
+    s.headers.update({'Authorization': 'Bearer %s' % token})
+
+    res = s.get('http://localhost:5000/calculate', json={'Te':300, 'Tc': '400a'})
+    if res.ok:
+        print(res.json())
+    else:
+        print(res.text)
