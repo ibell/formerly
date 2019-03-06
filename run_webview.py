f369ea49b80135121f7e5d4b04a9d1db83f3fd70
Ian Bell <ian.h.bell@gmail.com>
2019-03-05 19:57:10 -0700

    JWT lockdown fully working

--- run_webview.py
+++ run_webview.py
@@ -1,22 +1,44 @@
 from threading import Thread, Lock
 import logging
 import webview
+import requests
 from time import sleep
 import os
+import sys
 from flask_seasurf import SeaSurf
 
-server_lock = Lock()
 here = os.path.dirname(__file__)
-
 logger = logging.getLogger(__name__)
 
 from calculator import app, set_verify, verification_on
-set_verify(False)
+set_verify(True)
 
 def run_server():
+    """ This will run in its own thread """
     app.root_path = here
     app.run(host="127.0.0.1", port=23948, threaded=True)
     csrf = SeaSurf(app)
+
+def load_window():
+    # Load our page in the webview
+    webview.load_url("http://127.0.0.1:23948", uid='master')
+
+    # Now we retrieve the JWT token from the web service, and set it in the 
+    # window's javascript dict
+    res = requests.post('http://localhost:23948/login', json={"passkey":"trustme"})
+    if res.ok:
+        token = res.json()['access_token']
+    else:
+        print(res.text())
+        quit()
+
+    # Store the JWT token in the webview
+    webview.evaluate_js('pywebview = {{authToken: "{token:s}"}};'.format(token=token))
+
+    # Double-check we get back the right token
+    token_chk = webview.evaluate_js('pywebview.authToken')
+    if token_chk != token:
+        raise KeyError("JWT tokens didn't match")
 
 def url_ok(url, port):
     from http.client import HTTPConnection
@@ -40,6 +62,12 @@ if __name__ == '__main__':
         sleep(0.1)
 
     logger.debug("Server started")
-    webview.create_window("Formerly",
-                          "http://127.0.0.1:23948",
-                          min_size=(640, 480))
+    t = Thread(target=load_window)
+    t.daemon = True
+    t.start()
+    
+    uid = webview.create_window("Formerly",
+        min_size=(640, 480),
+        debug = True
+    )
+    sys.exit()
